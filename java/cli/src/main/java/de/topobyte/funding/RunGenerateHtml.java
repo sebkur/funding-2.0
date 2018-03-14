a5613f8782dec58d6dcdfbc46e8984aaaa12cd97
Sebastian Kürten <sebastian@topobyte.de>
2018-03-14 13:01:56 +0100

    HTML page: have more 'about' text

--- java/cli/src/main/java/de/topobyte/funding/RunGenerateHtml.java
+++ java/cli/src/main/java/de/topobyte/funding/RunGenerateHtml.java
@@ -148,7 +148,25 @@ public class RunGenerateHtml
 		content.ac(HTML.h1("Funding 2.0"));
 
 		P p = content.ac(HTML.p());
-		p.appendText("A crowd-sourced database of alternative funding sources");
+		p.appendText(
+				"A crowd-sourced database of alternative funding sources.");
+
+		p = content.ac(HTML.p());
+		p.appendText("Contribute to this collection on GitHub: ");
+		p.ac(HTML.a("https://github.com/sebkur/funding-2.0",
+				"sebkur/funding-2.0"));
+		p.appendText(".");
+
+		content.ac(HTML.h2("Credits"));
+		p = content.ac(HTML.p());
+
+		p.appendText("This project is based on a data set provided by the ");
+		p.ac(HTML.a("https://renewablefreedom.org/",
+				"Renewable Freedom Foundation"));
+		p.appendText(" that is available on GitHub: ");
+		p.ac(HTML.a("https://github.com/renewablefreedom/funding-sources",
+				"renewablefreedom/funding-sources"));
+		p.appendText(".");
 
 		htmlBuilder.write(path);
 	}
