64d9d655c1867e02681ec6747cb15fc6867441c0
Sebastian Kürten <sebastian@topobyte.de>
2018-03-23 17:41:03 +0100

    Build about page using markdown source

--- java/cli/src/main/java/de/topobyte/funding/html/AboutGenerator.java
+++ java/cli/src/main/java/de/topobyte/funding/html/AboutGenerator.java
@@ -3,10 +3,9 @@ package de.topobyte.funding.html;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import de.topobyte.funding.markdown.Markdown;
 import de.topobyte.jsoup.Bootstrap;
-import de.topobyte.jsoup.HTML;
 import de.topobyte.jsoup.HtmlBuilder;
-import de.topobyte.jsoup.components.P;
 import de.topobyte.jsoup.components.bootstrap3.Container;
 import de.topobyte.jsoup.nodes.Element;
 import de.topobyte.webpaths.WebPath;
@@ -24,28 +23,7 @@ public class AboutGenerator extends BaseGenerator
 
 		Container content = body.ac(Bootstrap.container());
 
-		content.ac(HTML.h1("Funding 2.0"));
-
-		P p = content.ac(HTML.p());
-		p.appendText(
-				"A crowd-sourced database of alternative funding sources.");
-
-		p = content.ac(HTML.p());
-		p.appendText("Contribute to this collection on GitHub: ");
-		p.ac(HTML.a("https://github.com/sebkur/funding-2.0",
-				"sebkur/funding-2.0"));
-		p.appendText(".");
-
-		content.ac(HTML.h2("Credits"));
-		p = content.ac(HTML.p());
-
-		p.appendText("This project is based on a data set provided by the ");
-		p.ac(HTML.a("https://renewablefreedom.org/",
-				"Renewable Freedom Foundation"));
-		p.appendText(" that is available on GitHub: ");
-		p.ac(HTML.a("https://github.com/renewablefreedom/funding-sources",
-				"renewablefreedom/funding-sources"));
-		p.appendText(".");
+		Markdown.renderResource(content, "about.md");
 
 		htmlBuilder.write(path);
 	}
---
+++ java/cli/src/main/resources/about.md
@@ -0,0 +1,13 @@
+# Funding 2.0
+
+A crowd-sourced database of alternative funding sources.
+
+Contribute to this collection on GitHub:
+[sebkur/funding-2.0](https://github.com/sebkur/funding-2.0).
+
+## Credits
+
+This project is based on a data set provided by the
+[Renewable Freedom Foundation](https://renewablefreedom.org/)
+that is available on GitHub:
+[renewablefreedom/funding-sources](https://github.com/renewablefreedom/funding-sources).
